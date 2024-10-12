a66d295ebf3cf7cb7b4a3aa5721785d6d62a2139
Manuel Nuñez <manuelisaias.nunez@estudiantes.unahur.edu.ar>
2024-10-12 14:49:18 -0300

    abm básico de productos

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,5 +1,6 @@
 const FabricanteControllers = require('./fabricante_controllers')
 const ComponenteControllers = require('./componente_controllers')
+const ProductoControllers = require('./producto_controllers')
 
 
-module.exports = {FabricanteControllers, ComponenteControllers}
+module.exports = {FabricanteControllers, ComponenteControllers, ProductoControllers}
