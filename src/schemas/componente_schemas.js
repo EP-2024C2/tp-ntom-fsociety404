549b87823f0bf4a68fc35b001d02a281bfa477f4
Manuel Nuñez <manuelisaias.nunez@estudiantes.unahur.edu.ar>
2024-10-20 21:16:41 -0300

    se agregan docker-compose para bd

--- src/schemas/componente_schemas.js
+++ src/schemas/componente_schemas.js
@@ -1,19 +1,13 @@
 const Joi = require("joi");
 
 const componenteSchema = Joi.object().keys({
-    nombre: Joi.string().trim().required().min(3).max(25).pattern(/^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/).messages({
+    nombre: Joi.string().trim().required().min(3).max(25).messages({
         "string.min": `El campo nombre debe tener al menos {#limit} caracteres`,
         "string.max": `El campo nombre debe tener como maximo {#limit} caracteres`,
         "string.empty": "El campo nombre no puede ser vacio",
-        "string.pattern.base": "El nombre solo debe contener letras",
-        "any.required": "El camponombre es requerido",
+        "any.required": "El campo nombre es requerido",
     }),
-    descripcion: Joi.string().trim().required().min(15).max(120).pattern(/^[a-zA-Z0-9\s.,]+$/).messages({
-        "string.min": `El campo descripcion debe tener al menos {#limit} caracteres`,
-        "string.max": `El campo descripcion debe tener como máximo {#limit} caracteres`,
-        "string.empty": "El campo descripcion no puede ser vacío",
-        "string.pattern.base": "El campo descripcion solo debe contener letras, números, espacios y los caracteres ., ",
-        "any.required": "El campo descripcion es requerido",
+    descripcion: Joi.string().messages({
     }),
 });
 
--- src/schemas/producto_schemas.js
+++ src/schemas/producto_schemas.js
@@ -1,20 +1,17 @@
 const Joi = require('joi')
 
-const conAcentos = /^[\wá-úÁ-Ú\s,.]+$/
 
 const productoSchema = Joi.object().keys({
-    nombre: Joi.string().trim().required().min(3).max(25).pattern(conAcentos).messages({
+    nombre: Joi.string().trim().required().min(3).max(25).messages({
         "string.min": `El campo nombre debe tener al menos {#limit} caracteres`,
         "string.max": `El campo nombre debe tener como maximo {#limit} caracteres`,
         "string.empty": "El campo nombre no puede ser vacio",
-        "string.pattern.base": "El nombre solo debe contener letras",
         "any.required": "El campo nombre es requerido",
     }),
-    descripcion: Joi.string().trim().required().min(15).max(120).pattern(conAcentos).messages({
+    descripcion: Joi.string().trim().required().min(15).max(120).messages({
         "string.min": `El campo descripcion debe tener al menos {#limit} caracteres`,
         "string.max": `El campo descripcion debe tener como máximo {#limit} caracteres`,
         "string.empty": "El campo descripcion no puede ser vacío",
-        "string.pattern.base": "El campo descripcion solo debe contener letras, números, espacios y los caracteres ., ",
         "any.required": "El campo descripcion es requerido",
     }),
 	precio: Joi.number().required().messages({
@@ -23,10 +20,7 @@ const productoSchema = Joi.object().keys({
 	}),
 	pathImg: Joi.string().required().uri().messages({
 		"string.uri": "El campo pathImg debe ser una URL válida",
-        "any.required": "El campo pathImg es requerido"
 	})
 });
 
 module.exports = productoSchema;
-
-// nombre - descripcion -  precio - pathImg
